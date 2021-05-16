d978c248e71ac28a1deb32c22a82ff8e467e822d
vtHau <crtrunghau@gmail.com>
2021-05-16 16:32:34 +0700

    add mess

--- inc/footer.php
+++ inc/footer.php
@@ -56,6 +56,29 @@
     </div>
   </div>
   <!-- Main Wrapper End Here -->
+  <!-- Messenger Plugin chat Code -->
+  <div id="fb-root"></div>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml: true,
+        version: 'v10.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
+
+  <!-- Your Plugin chat code -->
+  <div class="fb-customerchat" attribution="biz_inbox" page_id="100730118877099">
+  </div>
   </body>
   <script src="assets\js\vendor\modernizr-3.5.0.min.js"></script>
 
